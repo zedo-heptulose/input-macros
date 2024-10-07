0394673f2ad30509629bfc15488784ccd33c8815
zedo-heptulose <baylissgage@gmail.com>
2024-10-06 23:48:39 -0400

    proper OOP in orca_input.py

--- .ipynb_checkpoints/orca_input-checkpoint.py
+++ .ipynb_checkpoints/orca_input-checkpoint.py
@@ -5,19 +5,30 @@ import helpers
 
 CONFIGPATH = './'
 ORCACONFIG = 'orca_config.json'
+GAUSSCONFIG = 'gaussian_config.json'
 
-class OrcaInput:
-    def __init__(self):
+class Input:
+    def __init__(self): 
         self.path = ""
         self.filename = ""
         self.keywords = []
+        self.charge = 0
+        self.multiplicity = 1
+        self.xyzfile = "" 
+        #self.xyzpath = "" #what is this, and why?
+    
+    def cleanup(self):
+        raise NotImplementedError()
+
+    def write_file(self):
+        raise NotImplementedError()
+
+class ORCAInput(Input):
+    def __init__(self):
+        Input.__init__(self)
         self.strings = []
         self.blocks = {}
         
-        self.charge = 0
-        self.multiplicity = 1
-        self.xyzfile = ""
-
     def cleanup(self):
         self.keywords = [keyword for keyword in self.keywords if keyword]
         self.strings = [string for string in self.strings if string]
@@ -26,9 +37,8 @@ class OrcaInput:
     
     def write_file(self):
         self.cleanup()
-        if not self.path.endswith('/'):
-            self.path = f'{self.path.strip()}/'
-        with open (f'{self.path}{self.filename}.inp','w') as file:
+        full_path_basename = os.path.join(self.path,self.filename) 
+        with open (f'{full_path_basename}.inp','w') as file:
             for keyword in self.keywords:
                 file.write(f'! {keyword.strip()}\n')
             file.write('\n')
@@ -44,6 +54,34 @@ class OrcaInput:
             file.write(f"* xyzfile {self.charge} {self.multiplicity} {self.xyzfile} \n\n")
 
     
+class GaussianInput(Input):
+    def __init__(self):
+        Input.__init__(self)
+        self.nprocs = 1
+        self.mem_per_cpu_gb = 2
+        self.title = "created with so and so tool"
+
+    def cleanup(self):
+        self.keywords = [keyword for keyword in self.keywords if keyword]
+    
+    def write_file(self):
+        self.cleanup()
+        full_path_basename = os.path.join(self.path,self.filename)
+        with open(f'{self.xyzfile}','r') as xyzfile:
+            coordinates = xyzfile.readlines()[2:]
+        with open(f'{full_path_basename}.gjf','w') as gjffile:
+            gjffile.write(f"%nprocshared={self.nprocs}\n")
+            gjffile.write(f"%mem={self.nprocs*self.mem_per_cpu_gb}gb\n")
+            gjffile.write(f"%chk={full_path_basename}.chk\n")
+            gjffile.write(f"#{' '.join(self.keywords)}\n")
+            gjffile.write(f"\n")
+            gjffile.write(f"{self.title}\n")
+            gjffile.write(f"\n")
+            gjffile.write(f"{self.charge} {self.multiplicity}\n")
+            gjffile.writelines(coordinates)
+            gjffile.write(f"\n\n")
+
+
 class SbatchScript:
     def __init__(self):
         self.path = ""
@@ -66,7 +104,7 @@ class SbatchScript:
 class Job:
     def __init__(self):
         self.path = "./"
-        self.inp = OrcaInput() #or None, or other type
+        self.inp = Input() #or None, or other type #this throws an exception if not replaced
         self.sh = SbatchScript()
         self.xyzpath = "./"
         self.xyz = "test.xyz"
@@ -97,31 +135,27 @@ class Job:
 
 
 
-class ORCAInputBuilder:
+class InputBuilder:
     def __init__(self):
-            #defaults set here. In the future, uncouple from code
-            self.config = helpers.load_config_from_file(f'{CONFIGPATH}{ORCACONFIG}')
+        raise NotImplementedError('InputBuilder is an abstract class')
+        self.config = helpers.load_config_from_file('/path/to/config')
 
     def change_params(self,diff_config):
-        #change everything in self.config with diff_config.
-        #diff_config files should be sparse to reflect this
         for key in diff_config:
             self.config[key] = diff_config[key]
         return self
-    
-    def build(self):
-        newjob = Job()
-        
-        newjob.path = self.config['write_directory']
-        newjob.xyzpath = self.config['xyz_directory'] 
-        newjob.xyz = self.config['xyz_file']
 
-        ################## sh options ##################
-        
-        newjob.sh.path = self.config['write_directory']
-        
-        newjob.sh.filename = self.config['job_name']
-        newjob.sh.sbatch_statements = [
+    def build_input(self):
+        raise NotImplementedError()
+
+    def submit_line(self):
+        raise NotImplementedError()
+
+    def build_submit_script(self):
+        sh = SbatchScript()
+        sh.path = self.config['write_directory']
+        sh.filename = self.config['job_name']
+        sh.sbatch_statements = [
             f"--job-name={self.config['job_name']}",
             f"-n {self.config['num_cores']}",
             f"-N 1",
@@ -131,22 +165,43 @@ class ORCAInputBuilder:
         ]
         if self.config['pre_submit_lines'] is not None:
             for line in self.config['pre_submit_lines']:
-                newjob.sh.commands.append(line)
+                sh.commands.append(line) 
         
-        newjob.sh.commands.append(
-            f"{self.config['path_to_program']} {self.config['job_name']}.inp > {self.config['job_name']}.out"
-        )
+        sh.commands.append(self.submit_line())
 
         if self.config['post_submit_lines'] is not None:
             for line in self.config['post_submit_lines']:
-                newjob.sh.commands.append(line)
+                sh.commands.append(line)
+        return sh
 
-        ################ inp options #####################
 
-        newjob.inp.path = self.config['write_directory']
-        newjob.inp.filename = self.config['job_name']
+    def build(self):
+        newjob = Job()
         
-        newjob.inp.keywords = [
+        newjob.path = self.config['write_directory']
+        newjob.xyzpath = self.config['xyz_directory'] 
+        newjob.xyz = self.config['xyz_file']
+
+        newjob.sh = self.build_submit_script() 
+        newjob.inp = self.build_input()
+        
+        return newjob
+       
+    
+class ORCAInputBuilder(InputBuilder):
+    def __init__(self):
+        self.config = helpers.load_config_from_file(f'{CONFIGPATH}{ORCACONFIG}') 
+    
+    def submit_line(self):
+        return f"{self.config['path_to_program']} {self.config['job_name']}.inp > {self.config['job_name']}.out"
+    
+    def build_input(self):
+        ################ inp options #####################
+        inp = ORCAInput() 
+        inp.path = self.config['write_directory']
+        inp.filename = self.config['job_name']
+        
+        inp.keywords = [
             'UKS' if self.config['uks'] else None,
             self.config['functional'],
             self.config['basis'],
@@ -158,33 +213,63 @@ class ORCAInputBuilder:
             self.config['integration_grid'],
             self.config['run_type'],
         ]
-        newjob.inp.keywords.append(self.config['other_keywords'])
+        inp.keywords.append(self.config['other_keywords'])
 
         
         maxcore = int(self.config['mem_per_cpu_GB']) * 1000 * (3 / 4)
-        newjob.inp.strings.append(f"%maxcore {int(maxcore)}")
+        inp.strings.append(f"%maxcore {int(maxcore)}")
         if not self.config['blocks'].get('pal',None):
-            newjob.inp.blocks['pal'] = [f"nprocs {self.config['num_cores']}",]
+            inp.blocks['pal'] = [f"nprocs {self.config['num_cores']}",]
             
         for name in self.config['blocks']:
-            newjob.inp.blocks['name'] = self.config['blocks'][name]
+            inp.blocks['name'] = self.config['blocks'][name]
         
         if self.config['broken_symmetry']:
-            if not newjob.inp.blocks.get('scf',None):
-                newjob.inp.blocks['scf'] = ['brokensym 1,1']
+            if not inp.blocks.get('scf',None):
+                inp.blocks['scf'] = ['brokensym 1,1']
             else:
                 condition = False
-                for line in newjob.inp.blocks['scf']:
+                for line in inp.blocks['scf']:
                     if 'brokensym' in line.lower():
                         condition = True
                 if not condition:
-                    newjob.inp.blocks['scf'].append('brokensym 1,1')
+                    inp.blocks['scf'].append('brokensym 1,1')
         
-        newjob.inp.charge = self.config['charge']
-        newjob.inp.multiplicity = self.config['spin_multiplicity']
-        newjob.inp.xyzfile = self.config['xyz_file']
-        
-        #DONE
-        return newjob
-        
+        inp.charge = self.config['charge']
+        inp.multiplicity = self.config['spin_multiplicity']
+        inp.xyzfile = self.config['xyz_file']
+        return inp
 
+
+
+class GaussianInputBuilder(InputBuilder):
+    def __init__(self):
+        self.config = helpers.load_config_from_file(f"{CONFIGPATH}{GAUSSCONFIG}")
+
+    def submit_line(self):
+        return f"{self.config['path_to_program']} < {self.config['job_name']}.gjf > {self.config['job_name']}.log"
+
+    def build_input(self):
+        inp = GaussianInput()
+        #TODO: FIX THIS LATER
+
+        inp.path = self.config['write_directory']
+        inp.filename = self.config['job_name']
+        
+        inp.keywords = [
+            self.config['run_type'],
+            f"{'u' if self.config['uks'] else ''}{self.config['functional']}/{self.config['basis']}",
+            self.config['aux_basis'],
+            self.config['density_fitting'],
+            self.config['dispersion_correction'],
+            self.config['bsse_correction'],
+            self.config['integration_grid'],
+        ]
+        inp.keywords.append(self.config['other_keywords'])
+
+        nprocs = self.config['num_cores']
+        mem_per_cpu_gb = self.config['mem_per_cpu_GB']
+        inp.charge = self.config['charge']
+        inp.multiplicity = self.config['spin_multiplicity']
+        inp.xyzfile = os.path.join(self.config['xyz_directory'],self.config['xyz_file']) 
+        return inp
--- scratch/test/test.sh
+++ scratch/test/test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=test
-#SBATCH -n 12
+#SBATCH -n 20
 #SBATCH -N 1
 #SBATCH -p genacc_q
-#SBATCH -t 0-00:04:00
+#SBATCH -t 5-00:00:00
 #SBATCH --mem-per-cpu=4GB
 
-module purge
-module load gaussian16
-g16 < test.gjf > test.log
+module load gnu/8.5.0 openmpi/4.1.6 orca/6.0.0
+/gpfs/research/software/orca/orca_6_0_0_shared_openmpi416/orca test.inp > test.out
